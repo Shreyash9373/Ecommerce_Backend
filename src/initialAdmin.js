c0d2ba78bfee8f8ab26cd560ddb845c49e0322b6
shreyas <shreyashraut8@gmail.com>
2025-03-03 12:00:06 +0530

    InitialAdmin setup

--- src/initialAdmin.js
+++ src/initialAdmin.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config({
-  path: "../.env",
+  path: "./.env",
 });
 import mongoose from "mongoose";
 import { adminModel } from "./models/admin.model.js";
@@ -9,6 +9,7 @@ import connectDB from "./db/index.js";
 export const initialAdmin = async () => {
   try {
     console.log(" Connecting to MongoDB...");
+    console.log(process.env.MONGODB_URI);
     await connectDB(); // Ensure this is awaited
 
     console.log(" Checking if admin user exists...");
@@ -25,6 +26,11 @@ export const initialAdmin = async () => {
     await adminModel.create({
       email: "admin@gmail.com",
       password: process.env.adminPass, // Assuming password hashing is handled in the model
+      user: "Admin",
+      phone: 9373180080,
+      avatar:
+        "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png",
+      lastLogin: new Date(),
     });
 
     console.log(" Admin user created successfully.");
