35b9896508552bab6436b8257da929dc892821ad
shreyas <shreyashraut8@gmail.com>
2025-03-06 12:22:34 +0530

    Added Vendor routes for Admin

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -13,6 +13,12 @@ import {
   rejectProduct,
   getAllProducts,
   deleteProduct,
+  getAllVendors,
+  approveVendor,
+  rejectVendor,
+  deleteVendor,
+  getAllUsers,
+  getUser,
 } from "../controllers/admin.controller.js";
 import { verifyJwtAdmin } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
@@ -31,7 +37,7 @@ router.post(
 router.delete("/deleteCategory/:id", verifyJwtAdmin, deleteCategory);
 router.delete("/deleteSubCategory/:id", verifyJwtAdmin, deleteSubCategory);
 router.delete(
-  "/deleteAllSubCategories",
+  "/deleteAllSubCategories/:id",
   verifyJwtAdmin,
   deleteAllSubCategories
 );
@@ -50,5 +56,15 @@ router.put("/approveProduct/:productId", verifyJwtAdmin, approveProduct);
 router.put("/rejectProduct/:productId", verifyJwtAdmin, rejectProduct);
 router.get("/getAllProducts", verifyJwtAdmin, getAllProducts);
 router.delete("/deleteProduct/:productId", verifyJwtAdmin, deleteProduct);
+
+//Vendor Routes
+router.get("/getAllVendors", verifyJwtAdmin, getAllVendors);
+router.put("/approveVendor/:vendorId", verifyJwtAdmin, approveVendor);
+router.put("/rejectVendor/:vendorId", verifyJwtAdmin, rejectVendor);
+router.delete("/deleteVendor/:vendorId", verifyJwtAdmin, deleteVendor);
+
+//User Routes
+router.get("/getAllUsers", verifyJwtAdmin, getAllUsers);
+router.get("/getUser/:userId", verifyJwtAdmin, getUser);
 
 export default router;
