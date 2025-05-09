174014d968e75296bf81eb4ca92e00fb5bddbda3
jajgaonkar <jayajgaonkar20@gmail.com>
2025-05-10 03:12:02 +0530

    product.controller and order.controller

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwtVendor } from "../middlewares/vendor.middleware.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import {
   addProduct,
   getAllVendorProducts,
@@ -9,6 +10,8 @@ import {
   updateProductById,
   getAllApprovedProducts,
   searchProducts,
+  getCategories,
+  getSubCategories,
 } from "../controllers/products.controller.js";
 import { verifyJwtUser } from "../middlewares/user.middleware.js";
 import verifyJwtVendorOrAdmin from "../middlewares/adminorvendor.middleware.js";
@@ -41,7 +44,7 @@ router.route("/update-product/:productId").put(
 router.route("/getAll-Products").get(verifyJwtVendor, getAllVendorProducts);
 router.route("/get-product/:productId").get(getProductById);
 
-router.route("/search-products").post(searchProducts);
+router.route("/search-products").get(searchProducts);
 
 // Open route to get products for showing in main site
 router.route("/get-approvedProducts").get(getAllApprovedProducts);
@@ -49,5 +52,8 @@ router.route("/get-approvedProducts").get(getAllApprovedProducts);
 router
   .route("/delete-product/:productId")
   .delete(verifyJwtVendor, deleteProductById);
+
+router.route("/categories").get(getCategories);
+router.route("/subcategories").get(getSubCategories);
 
 export default router;
